da7d36dc3f74732a549c3e2459c087fe42937105
Abhinav Omprakash <abhinav@teamohana.com>
2022-02-07 09:16:43 +0530

    release: create release 0.0.1

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # snitch
 
+[![Clojars Project](https://img.shields.io/clojars/v/org.clojars.abhinav/snitch.svg)](https://clojars.org/org.clojars.abhinav/snitch)
+
 A Clojure library designed to ... well, that part is up to you.
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject snitch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject org.clojars.abhinav/snitch "0.0.1"
+  :description "Tools to gain insight into the data inside functions."
+  :url "https://github.com/AbhinavOmprakash/snitch"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  ]
   :plugins [[lein-auto "0.1.3"]
             [lein-cloverage "1.0.9"]]
-  :repl-options {:init-ns snitch.core})
+  :repl-options {:init-ns snitch.core}
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                            :sign-releases false}]])
