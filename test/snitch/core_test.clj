1ae94a5865b3c5ab81f5209e3325ecd245bd735f
Abhinav Omprakash <abhinav@teamohana.com>
2022-02-19 10:28:33 +0530

    refactor: remove suffix and prefix

--- test/snitch/core_test.clj
+++ test/snitch/core_test.clj
@@ -1,7 +1,7 @@
 (ns snitch.core-test
   (:require
     [clojure.test :refer :all]
-    [snitch.core :refer [concat-symbols arg->def-args define-args define-let-bindings defn* defn** defn***]]
+    [snitch.core :refer [concat-symbols arg->def-args define-args define-let-bindings defn*]]
     [snitch.test-utils]))
 
 
@@ -18,64 +18,42 @@
 
 (deftest test-arg->def-args
   (testing "simple symbol"
-    (is (expansion-valid? (arg->def-args 'a) (def a a)))
-    (is (expansion-valid? (arg->def-args "" 'a) (def a a)))
-    (is (expansion-valid? (arg->def-args "*" 'a) (def a* a)))
-    (is (expansion-valid? (arg->def-args  'fname "*" 'a) (def fname-a* a)))
-    (is (expansion-valid? (arg->def-args  "fname" "*" 'a) (def fname-a* a))))
-
+    (is (expansion-valid? (arg->def-args 'a) (def a a))))
 
   (testing "Vector Destructuring"
-    (is (expansion-valid? (arg->def-args "" ['a 'b])
+    (is (expansion-valid? (arg->def-args ['a 'b])
                           ((def a a) (def b b))))
 
-    (is (expansion-valid? (arg->def-args "*" ['a 'b])
-                          ((def a* a) (def b* b)))))
+    (testing "Map Destructuring"
+      (is (expansion-valid? (arg->def-args {:keys ['a 'b]})
+                            ((def a a) (def b b))))
 
-  (testing "Map Destructuring"
-    (is (expansion-valid? (arg->def-args "" {:keys ['a 'b]})
-                          ((def a a) (def b b))))
-    (is (expansion-valid? (arg->def-args "*" {:keys ['a 'b]})
-                          ((def a* a) (def b* b))))
+      (is (expansion-valid? (arg->def-args {:keys ['a 'b] :as 'c})
+                            ((def a a) (def b b) (def c c))))
 
-    (is (expansion-valid? (arg->def-args "" {:keys ['a 'b] :as 'c})
-                          ((def a a) (def b b) (def c c))))
-    (is (expansion-valid? (arg->def-args "*" {:keys ['a 'b] :as 'c})
-                          ((def a* a) (def b* b) (def c* c))))
-
-    (is (expansion-valid? (arg->def-args "" {'idx 'index :keys ['a 'b]})
-                          ((def idx idx) (def a a) (def b b))))
-    (is (expansion-valid? (arg->def-args "*" {'idx 'index :keys ['a 'b]})
-                          ((def idx* idx) (def a* a) (def b* b))))
-    (is (expansion-valid? (arg->def-args {:keys ['ns/foo]})
-                          ((def foo foo)))
-        "Should be able to def qualified symbols")))
+      (is (expansion-valid? (arg->def-args {'idx 'index :keys ['a 'b]})
+                            ((def idx idx) (def a a) (def b b))))
+      (is (expansion-valid? (arg->def-args {:keys ['ns/foo]})
+                            ((def foo foo)))
+          "Should be able to def qualified symbols"))))
 
 
 (deftest test-define-args
   (testing "Params list with simple symbols"
     (is (expansion-valid? (define-args ['a 'b])
-                          ((def a a) (def b b))))
-    (is (expansion-valid? (define-args "*" ['a 'b])
-                          ((def a* a) (def b* b)))))
+                          ((def a a) (def b b)))))
 
   (testing "Params list with vector destructuring"
     (is (expansion-valid? (define-args ['a ['b 'c]])
-                          ((def a a) (def b b) (def c c))))
-    (is (expansion-valid? (define-args "*" ['a ['b 'c]])
-                          ((def a* a) (def b* b) (def c* c)))))
+                          ((def a a) (def b b) (def c c)))))
 
   (testing "Params list with map destructuring"
     (is (expansion-valid? (define-args ['x {:keys ['a 'b] :as 'c}])
-                          ((def x x) (def a a) (def b b) (def c c))))
-    (is (expansion-valid? (define-args "*" ['x {:keys ['a 'b] :as 'c}])
-                          ((def x* x) (def a* a) (def b* b) (def c* c)))))
+                          ((def x x) (def a a) (def b b) (def c c)))))
 
   (testing "Params list with simple symbols, vector and map destructuring"
     (is (expansion-valid? (define-args ['x ['j 'k] {:keys ['a 'b] :as 'c}])
-                          ((def x x) (def j j) (def k k) (def a a) (def b b) (def c c))))
-    (is (expansion-valid? (define-args "*" ['x ['j 'k] {:keys ['a 'b] :as 'c}])
-                          ((def x* x) (def j* j) (def k* k) (def a* a) (def b* b) (def c* c))))))
+                          ((def x x) (def j j) (def k k) (def a a) (def b b) (def c c))))))
 
 
 (deftest test-define-let-bindings
@@ -134,64 +112,7 @@
                             (let [a 1 _ (def a a)] (print a))
                             (let [b 2 _ (def b b)] (str b))
                             (let [c 3 _ (def c c)] (symbol c))))))
-  (testing "let with suffixes"
-    (is (expansion-valid? (define-let-bindings "-" '(let [a 1] (print a)))
-                          (let [a 1 _ (def a- a)] (print a))))
-    (is (expansion-valid? (define-let-bindings "-" '(let [a 1] (print a) (let [b 2] b)))
-                          (let
-                            [a 1 _ (def a- a)]
-                            (print a)
-                            (let [b 2 _ (def b-- b)] b)))
-        "each nested let should have an additional suffix to it. 
-                            a is in level one so it is a-
-                            b is in level 2 so it is b--")
-    (is (expansion-valid? (define-let-bindings "-" '(let [a 1]
-                                                      (print a)
-                                                      (let [b 2]
-                                                        (str b)
-                                                        (let [b 2] b))))
-                          (let
-                            [a 1 _ (def a- a)]
-                            (print a)
-                            (let
-                              [b 2 _ (def b-- b)]
-                              (str b)
-                              (let [b 2 _ (def b--- b)] b)))))
-    (is (expansion-valid? (define-let-bindings "-" '(do (let [a 1] (print a))
-                                                        (let [b 2] (str b))
-                                                        (let [c 3] (symbol c))))
 
-                          (do
-                            (let [a 1 _ (def a- a)] (print a))
-                            (let [b 2 _ (def b- b)] (str b))
-                            (let [c 3 _ (def c- c)] (symbol c))))
-        "This asserts that when let has the same level (non nested) then they should have the same suffix."))
-
-
-
-
-
-
-
-  (testing "let with prefixes and suffixes"
-    (is (expansion-valid? (define-let-bindings "fname" "-" '(let [a 1] (print a)))
-                          (let [a 1 _ (def fname-a- a)] (print a))))
-    (is (expansion-valid? (define-let-bindings "fname" "-" '(let [a 1] (print a) (let [b 2] b)))
-                          (let
-                            [a 1 _ (def fname-a- a)]
-                            (print a)
-                            (let [b 2 _ (def fname-b-- b)] b)))
-        "each nested let should have an additional suffix to it, but all should have only one suffix
-                            a is in level one so it is fname-a-
-                            b is in level 2 so it is  fname-b--")
-    (is (expansion-valid? (define-let-bindings "fname"  "-" '(do (let [a 1] (print a))
-                                                                 (let [b 2] (str b))
-                                                                 (let [c 3] (symbol c))))
-
-                          (do
-                            (let [a 1 _ (def fname-a- a)] (print a))
-                            (let [b 2 _ (def fname-b- b)] (str b))
-                            (let [c 3 _ (def fname-c- c)] (symbol c))))))
   (testing "let with destructuring"
     (is (expansion-valid? (define-let-bindings '(let [[a b] [1 2]
                                                       c 3]))
@@ -202,7 +123,7 @@
                                 _ (def c c)])))))
 
 
-#_(deftest test-defn*
+(deftest test-defn*
   (testing "defn* with name, params and body"
     (is (macro-valid? (defn* hey [x]
                         x)
@@ -309,7 +230,3 @@
                           [result__12833__auto__ {:val x}]
                           (def hey> result__12833__auto__) ; test will fail because of autogensym
                           result__12833__auto__))))))
-
-
-
-
