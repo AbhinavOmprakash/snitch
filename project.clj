8da4f37613865c6033630286453fb66202a85266
Abhinav Omprakash <abhinav@teamohana.com>
2022-02-21 19:07:42 +0530

    chore: bump version

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject org.clojars.abhinav/snitch "0.0.6"
+(defproject org.clojars.abhinav/snitch "0.0.7"
   :description "Tools to gain insight into the data inside functions."
   :url "https://github.com/AbhinavOmprakash/snitch"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :plugins [[lein-auto "0.1.3"]
             [lein-cloverage "1.0.9"]]
   :repl-options {:init-ns snitch.core}
   :repositories [["releases" {:url "https://repo.clojars.org"
-                            :sign-releases false}]])
+                              :sign-releases false}]])
