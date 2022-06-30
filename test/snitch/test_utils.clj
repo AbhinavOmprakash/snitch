d52cdc52e00ec83879081ee497a5997af1d159eb
Abhinav Omprakash <abhinav@teamohana.com>
2022-06-30 20:37:56 +0530

    feat: convert to simple symbol in expansion-valid?

--- test/snitch/test_utils.clj
+++ test/snitch/test_utils.clj
@@ -4,23 +4,6 @@
     [clojure.test :refer [assert-expr do-report]]))
 
 
-;; can be used with functions that return quoted lists or macros
-(defmethod assert-expr 'expansion-valid? [msg form]
-  (let [[_ macro expected-expansion] form]
-    ;; (is (expansion-valid? (macro arg) expansion)) 
-    ;; (is (expansion-valid? (fn arg) expansion))
-    `(let [result# ~(= (eval macro) expected-expansion)]
-       (if result#
-         (do-report {:actual '~(eval macro)
-                     :expected '~expected-expansion
-                     :message ~msg
-                     :type :pass})
-         (do-report {:actual '~(eval macro)
-                     :expected '~expected-expansion
-                     :message ~msg
-                     :type :fail})))))
-
-
 (defn binding-form?
   [form]
   (boolean
@@ -30,21 +13,28 @@
 
 
 (defn autogensym->sym
-  "Converts autogensymed symbols to simple symbols.
+  "Converts autogensymed or gensymed symbols to simple symbols.
   This makes it simple to compare two macro expansions.
   
   (autogensym->sym 'end_result__12833__auto__) 
   ;=> end_result
   "
   [autogensym-symbol]
+  ; the reason I'm doing the replace in 2 steps is because
+  ; gensym generates a symbol like p__123123
+  ; and it doesn't have the __auto__ part.
   (let [sym (str autogensym-symbol)]
-    (symbol (s/replace sym #"__\d.*__auto__" ""))))
+    (-> sym
+        (s/replace #"__auto__" "")
+        (s/replace #"__\d.*" "")
+    symbol)))
 
 
 (defn autogensym?
-  "Returns true if symbol is an autogensym symbol"
+  "Returns true if symbol is an autogensym or gensym symbol"
   [x]
-  (boolean (re-matches #".*__\d.*__auto__" (str x))))
+  (boolean (or (re-matches #".*__\d.*__auto__" (str x))
+               (re-matches #".*__\d*" (str x)))))
 
 
 (def seq-or-list-or-vector? (some-fn vector? list? seq?))
@@ -67,6 +57,24 @@
             ()
             [])
           form))
+
+
+;; can be used with functions that return quoted lists or macros
+(defmethod assert-expr 'expansion-valid? [msg form]
+  (let [[_ macro expected-expansion] form]
+    ;; (is (expansion-valid? (macro arg) expansion)) 
+    ;; (is (expansion-valid? (fn arg) expansion))
+    `(let [result# ~(= (replace-autogensym-symbols  (eval macro))
+                       (replace-autogensym-symbols expected-expansion))]
+       (if result#
+         (do-report {:actual '~(replace-autogensym-symbols  (eval macro))
+                     :expected '~(replace-autogensym-symbols   expected-expansion)
+                     :message ~msg
+                     :type :pass})
+         (do-report {:actual '~(replace-autogensym-symbols  (eval macro))
+                     :expected '~(replace-autogensym-symbols   expected-expansion)
+                     :message ~msg
+                     :type :fail})))))
 
 
 (defmethod assert-expr 'macro-valid? [msg form]
