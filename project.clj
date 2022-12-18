2870eb9e7ebd47fe1d6cc0cc9a379e6108e1adb0
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2022-12-18 10:36:49 +0530

    chore: bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject org.clojars.abhinav/snitch "0.1.12-SNAPSHOT"
-  :description "A repl-based, editor-agnostic, clojure and clojurescript debugger.
-               In other words, inline-defs on steroids."
+(defproject org.clojars.abhinav/snitch "0.1.13"
+  :description
+  "Snitch injects inline defs in your functions and multimethods.
+                This enables a repl-based, editor-agnostic, clojure and clojurescript debugging workflow. 
+                It is inline-defs on steroids."
   :url "https://github.com/AbhinavOmprakash/snitch"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -11,3 +13,4 @@
             [lein-eftest "0.5.9"]]
   :repositories [["releases" {:url "https://repo.clojars.org"
                               :sign-releases false}]])
+
